9b96053125275fe11f89d2accc2ca50045cf0e64
Caleb Cushing <xenoterracide@gmail.com>
2013-05-07 22:35:13 -0500

    redo synopsis
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Pod/Spell.pm
+++ lib/Pod/Spell.pm
@@ -266,13 +266,20 @@ sub _treat_words {    ## no critic ( Subroutines::RequireArgUnpacking )
 
 =head1 SYNOPSIS
 
-  % podspell Thing.pm | ispell
- or if you don't have a podspell:
-  % perl -MPod::Spell -e "Pod::Spell->new->parse_from_file(shift)" Thing.pm |spell |fmt
+	use Pod::Spell;
+	Pod::Spell->new->parse_from_file( 'File.pm' );
 
- or:
-  % perl -MPod::Spell -e "Pod::Spell->new->parse_from_filehandle"
-  ...which takes POD on STDIN and sends formatted text to STDOUT
+	Pod::Spell->new->parse_from_filehandle( $fh );
+
+Also look at L<podspell>
+
+	% perl -MPod::Spell -e "Pod::Spell->new->parse_from_file(shift)" Thing.pm |spell |fmt
+
+or:
+
+	% perl -MPod::Spell -e "Pod::Spell->new->parse_from_filehandle"
+
+...which takes POD on STDIN and sends formatted text to STDOUT
 
 ...or instead of piping to spell or C<ispell>, use C<E<gt>temp.txt>, and open
 F<temp.txt> in your word processor for spell-checking.
