3703d47ca8a5e12479033bccc7f89b7ece5de522
Caleb Cushing <xenoterracide@gmail.com>
2013-05-08 01:46:09 -0500

    Build results of 2f31ea5 (on master)

--- lib/Pod/Spell.pm
+++ lib/Pod/Spell.pm
@@ -62,11 +62,11 @@ sub _get_stopwords_from {
 	while ( $text =~ m<(\S+)>g ) {
 		my $word = $1;
 		if ( $word =~ m/^!(.+)/s ) { # "!word" deletes from the stopword list
-			delete $stopwords->{$word};
+			delete $stopwords->{$1};
 			print "Unlearning stopword $word\n" if $self->_is_debug;
 		}
 		else {
-			$stopwords->{$1} = 1;
+			$stopwords->{$word} = 1;
 			print "Learning stopword $1\n" if $self->_is_debug;
 		}
 	}
