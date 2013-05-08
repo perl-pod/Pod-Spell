9f859f25744ed07cfe8456dfcc77610de404ea28
Caleb Cushing <xenoterracide@gmail.com>
2013-05-07 21:35:21 -0500

    Build results of 13518c1 (on master)

--- lib/Pod/Spell.pm
+++ lib/Pod/Spell.pm
@@ -39,11 +39,11 @@ sub verbatim { return ''; } # totally ignore verbatim sections
 #----------------------------------------------------------------------
 
 sub _get_stopwords_from { ## no critic ( Subroutines::RequireArgUnpacking )
-  my $self = shift;
+  my ( $self, $arg ) = @_;
   my $stopwords = $self->{'spell_stopwords'};
 
   my $word;
-  while($_[1] =~ m<(\S+)>g) {
+  while( $arg =~ m<(\S+)>g) {
     $word = $1;
     if($word =~ m/^!(.+)/s) {  # "!word" deletes from the stopword list
       delete $stopwords->{$1};
@@ -54,7 +54,6 @@ sub _get_stopwords_from { ## no critic ( Subroutines::RequireArgUnpacking )
     }
   }
   return;
-
 }
 
 #----------------------------------------------------------------------
